113079cc636aa1b55095f856bffb9e99e6b55d52
Stefan van der Walt <stefan@sun.ac.za>
2011-11-29 19:21:27 -0800

    Do not override NumPy's KnownFailure class explicitly.

--- IPython/external/decorators/__init__.py
+++ IPython/external/decorators/__init__.py
@@ -3,7 +3,7 @@ try:
     from numpy.testing.noseclasses import KnownFailure
 except ImportError:
     from _decorators import *
-    try:
-        from _numpy_testing_noseclasses import KnownFailure
-    except ImportError:
-        pass
+    ## try:
+    ##     from _numpy_testing_noseclasses import KnownFailure
+    ## except ImportError:
+    ##     pass
